e666ee3dd85262d148732b6af7d9337fdde0c8a3
AshuManit <79254688+AshuManit@users.noreply.github.com>
2023-03-21 16:38:47 +0530

    Update CheckValid.java

--- task2/CheckValid.java
+++ task2/CheckValid.java
@@ -1,9 +1,33 @@
 import java.util.Scanner;
-class CheckValid{
+import java.util.regex.*;
+/*
+It can be only six digits.
+It should not start with zero.
+First digit of the pin code must be from 1 to 9.
+Next five digits of the pin code may range from 0 to 9.
+It should allow only one white space, but after three digits, although this is optional.*/
+class ValidPin{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
-		int pin=sc.nextInt();
-		String res=(pin==443122)?"Valid":"Invalid";
-		System.out.println(res);
+		String regex= "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+			/*
+			^ represents the starting of the number.
+			[1-9]{1} represents the starting digit in the pin code ranging from 1 to 9.
+			[0-9]{2} represents the next two digits in the pin code ranging from 0 to 9.
+			\\s{0, 1} represents the white space in the pin code that can occur once or never.
+			[0-9]{3} represents the last three digits in the pin code ranging from 0 to 9.
+			$ represents the ending of the number.
+			*/
+	    Pattern p = Pattern.compile(regex);
+		String pin=sc.nextLine();
+		if(pin==null){
+			System.out.println("Invalid");
+		}
+		Matcher match=p.matcher(pin); //Matcher contains method matcher 
+		if(match.matches()) //matches() return boolean 
+			System.out.println("Valid Pin");
+		else 
+			System.out.println("Invalid pin");
+
 	}
 }
