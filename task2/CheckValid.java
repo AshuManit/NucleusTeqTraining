ad12901f400fe37a2d625cd561cbdd0606b843f2
AshuManit <79254688+AshuManit@users.noreply.github.com>
2023-03-21 17:03:19 +0530

    Update and rename CheckValid.java to CheckValidPin.java

--- task2/CheckValid.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class CheckValid{
-	public static void main(String args[]){
-		Scanner sc=new Scanner(System.in);
-		int pin=sc.nextInt();
-		String res=(pin==443122)?"Valid":"Invalid";
-		System.out.println(res);
-	}
-}
