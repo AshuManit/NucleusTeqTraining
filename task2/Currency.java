d54a1b7ed978a27f02656809074949de53cd5755
AshuManit <79254688+AshuManit@users.noreply.github.com>
2023-03-21 17:02:46 +0530

    Update Currency.java

--- task2/Currency.java
+++ task2/Currency.java
@@ -1,10 +1,34 @@
 import java.util.Scanner;
 public class Currency{
+	public static double toDollar(double rs){
+		double dol=rs/82.63;
+		return dol;
+	}
+	public static double torupees(double dol){
+		double rs=dol*82.63;
+		return rs;
+	}
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
+		System.out.println("press 1 to convert Ruppes to Dollar");
+		System.out.println("press 1 to convert Dollar to Ruppes");
 		String input=sc.nextLine();
 		switch(input){
-			case "rs":System.out.println("$");
+			case 1:
+					System.out.println("Enter Amount in rupees");
+					double rs=sc.nextDouble();
+					double dol=toDollar(rs);
+					System.out.println(dol+"$");
+					break;
+			case 2: 
+			
+					System.out.println("Enter Amount in Dollar");
+					double dol=sc.nextDouble();
+					double rs=torupees(dol);
+					System.out.println(rs+"rs");
+					break;
+					default :
+					System.out.println("Wrong pressed");
 		}
 	}
 }
