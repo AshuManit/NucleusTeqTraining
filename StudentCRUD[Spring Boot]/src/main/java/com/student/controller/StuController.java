d4966cc858f29f84f0383a0d3dc41e98c9b68c57
AshuManit <79254688+AshuManit@users.noreply.github.com>
2023-04-14 11:03:42 +0530

    Update StuController.java

--- StudentCRUD[Spring Boot]/src/main/java/com/student/controller/StuController.java
+++ StudentCRUD[Spring Boot]/src/main/java/com/student/controller/StuController.java
@@ -1,11 +1,16 @@
 package com.student.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.student.model.Student;
 import com.student.service.StuService;
@@ -22,23 +27,24 @@ public class StuController {
 	}
 	
 	@RequestMapping(method=RequestMethod.PUT , value="/home/{id}")
-	public void update(@RequestBody Student student,@PathVariable Integer id) {
+	public void update(@RequestBody Student student,@PathVariable String id) {
 		stuService.updateStudent(student,id);
 	}
 	
-	@RequestMapping(method=RequestMethod.DELETE , value="/home/{id}s")
-	public void delete(Student student,@PathVariable Integer id) {
+	@RequestMapping(method=RequestMethod.DELETE , value="/home/{id}")
+	public void delete(Student student,@PathVariable String id) {
 		stuService.deleteStudent(id);
 	}
 	
 	@RequestMapping(method=RequestMethod.GET , value="/home")
-	public void getAllStudent(Student student) {
-		stuService.getAllStudent();
+	public List<Student> getAllStudent() {
+		return stuService.getAllStudent();
+		
 	}
 	
 	@RequestMapping(method=RequestMethod.GET , value="/home/{id}")
-	public void getStudentById(Student student,@PathVariable Integer id) {
-		stuService.getStudentById(id);
+	public Optional<Student> getStudentById(@PathVariable String id) {
+		return stuService.getStudentById(id);
 	}
 	
 }
