20e299091c2584774f9fdc92f4fd1b650438ce1c
AshuManit <79254688+AshuManit@users.noreply.github.com>
2023-04-26 13:26:17 +0530

    Add files via upload

---
+++ logDemo-1/src/main/java/com/example/demo/LogDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogDemo1Application.class, args);
+	}
+
+}
---
+++ logDemo-1/src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+	
+	private static final Logger logger= Logger.getLogger(IndexController.class);
+	
+	//instance initializer block
+	{
+		BasicConfigurator.configure();
+	}
+	@GetMapping("/")
+	public String index() {
+		logger.debug("loading index page..");
+		logger.info("info level message!!");
+		logger.warn("WARN level message !!");
+		logger.error("Error level message !!");
+		logger.fatal("FATAL level message !!");
+		logger.trace("TRACE level message!!");
+		logger.debug("exit of index(");
+		return "index";
+	}
+}
